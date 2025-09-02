5b2cbc87c4ed2dbe640eb7e1610fb00b39fdc769
kissagnesa <sindzse88@gmail.com>
2025-09-02 17:33:58 +0200

    admin_interface done

--- modules/admin_interface.php
+++ modules/admin_interface.php
@@ -1,3 +1,15 @@
 <?php
-
+if(!isset($_SESSION["user"]))
+{
+?>
+<a href="index.php?action=login.php"></a>
+<?php
+}
+else{
+?>
+<i></i>
+<?php
+echo("&nbsp;<DIV>".$_SESSION["user"]["email"]."</DIV>&nbsp;");
+echo("<a class='usergomb' href='index.php?logout=1'>Kilépés</a>");
+}
 ?>
